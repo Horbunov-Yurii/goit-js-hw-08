58ae10700898cec534df9bb8d7a895dca582bb52
Horbunov Yurii <urijnet@gmail.com>
2023-07-24 20:49:24 +0300

    tasck-1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,10 +1,43 @@
+
+import SimpleLightbox from 'simplelightbox';
+
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-// Описаний в документації
-import SimpleLightbox from "simplelightbox";
-// Додатковий імпорт стилів
-import "simplelightbox/dist/simple-lightbox.min.css";
 
-console.log(galleryItems);
+
+
+const listRef = document.querySelector('.gallery');
+
+const marcup = galleryItems
+  .map(
+    ({ description, preview, original }) =>
+      `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>`
+  )
+  .join('');
+
+listRef.insertAdjacentHTML('beforeend', marcup);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+
+`<li class="gallery__item">
+    <a class="gallery__link" href="${galleryItems.original}">
+    <img class="gallery__image" src="${galleryItems.preview}" alt="${galleryItems.description}" />
+  </a>
+</li>`;
+
