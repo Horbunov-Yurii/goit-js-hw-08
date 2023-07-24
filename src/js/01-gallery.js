8a3cb15492f86c8e6a20bc02a5c49903cbb57cc5
Horbunov Yurii <urijnet@gmail.com>
2023-07-24 15:32:21 +0300

    last-push

---
+++ src/js/01-gallery.js
@@ -0,0 +1,10 @@
+// Add imports above this line
+import { galleryItems } from './gallery-items';
+// Change code below this line
+
+// Описаний в документації
+import SimpleLightbox from "simplelightbox";
+// Додатковий імпорт стилів
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+console.log(galleryItems);
