bea3b77faa383eaa15058ba1aee935afc8bb1de3
Horbunov Yurii <urijnet@gmail.com>
2023-07-25 16:17:41 +0300

    tasck-2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,19 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+
+
+    const iframe = document.querySelector('iframe');
+    const player = new Player(iframe);
+
+   const STORAGE_KEY = 'videoplayer-current-time';
+
+  function onTimeUpdate(data){
+ localStorage.setItem(STORAGE_KEY, `${data.seconds}`);
+  };
+
+  player.on('timeupdate', throttle(onTimeUpdate, 1000));
+
+
+player.setCurrentTime(Number(localStorage.getItem(STORAGE_KEY)));
+ 
